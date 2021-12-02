00f6360a30820a45b504ee8625f3f15dae68ca6f
Marco Pérez <marcopza99@hotmail.com>
2021-12-01 22:19:22 -0500

    Add password verification

--- src/main/java/com/example/filecipher/CipherDecipherController.java
+++ src/main/java/com/example/filecipher/CipherDecipherController.java
@@ -2,8 +2,15 @@ package com.example.filecipher;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.Objects;
 
 public class CipherDecipherController {
 
@@ -27,22 +34,59 @@ public class CipherDecipherController {
 
     @FXML
     void cipherBtnPressed(ActionEvent event) {
-
+        if (cipherPass.getText().equals(cipherPassConfirm.getText())) {
+            // TODO()
+        } else {
+            showAlert();
+        }
     }
 
     @FXML
     void decipherBtnPressed(ActionEvent event) {
-
+        if (decipherPass.getText().equals(decipherPassConfirmed.getText())) {
+            // TODO()
+        } else {
+            showAlert();
+        }
     }
 
     @FXML
-    void findChipherBtnPressed(ActionEvent event) {
-
+    void findCipherBtnPressed(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Escoja el archivo a cifrar");
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        File file = fileChooser.showOpenDialog(stage);
+        cipherFileAddress.setText(file.getAbsolutePath());
     }
 
     @FXML
     void findDecipherBtnPressed(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Escoja el archivo a descifrar");
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        File file = fileChooser.showOpenDialog(stage);
+        decipherFileAddress.setText(file.getAbsolutePath());
+    }
 
+    private void showAlert() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("Error");
+        alert.showAndWait();
+        cleanFields();
+    }
+
+    private void cleanFields() {
+
+        // Cifrar
+        cipherFileAddress.setText("");
+        cipherPass.setText("");
+        cipherPassConfirm.setText("");
+
+        // Descrifrar
+        decipherFileAddress.setText("");
+        decipherPass.setText("");
+        decipherPassConfirmed.setText("");
     }
 
 }
