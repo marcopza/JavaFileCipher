ce5d4a170e15aa4b609d1fba997002d1a763791f
Marco Pérez <marcopza99@hotmail.com>
2021-12-02 20:36:25 -0500

    Add encryption and decryption

--- src/main/java/com/example/filecipher/CipherDecipherController.java
+++ src/main/java/com/example/filecipher/CipherDecipherController.java
@@ -9,8 +9,16 @@ import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.util.Objects;
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 
 public class CipherDecipherController {
 
@@ -34,8 +42,9 @@ public class CipherDecipherController {
 
     @FXML
     void cipherBtnPressed(ActionEvent event) {
-        if (cipherPass.getText().equals(cipherPassConfirm.getText())) {
-            // TODO()
+        if ((cipherPass.getText().equals(cipherPassConfirm.getText()) && !cipherPass.getText().equals(""))) {
+            File toEncrypt = new File(cipherFileAddress.getText());
+            encryptDecrypt(toEncrypt, cipherPass.getText(), Cipher.ENCRYPT_MODE);
         } else {
             showAlert();
         }
@@ -43,8 +52,9 @@ public class CipherDecipherController {
 
     @FXML
     void decipherBtnPressed(ActionEvent event) {
-        if (decipherPass.getText().equals(decipherPassConfirmed.getText())) {
-            // TODO()
+        if ((decipherPass.getText().equals(decipherPassConfirmed.getText()) && !decipherPass.getText().equals(""))) {
+            File toDecrypt = new File(decipherFileAddress.getText());
+            encryptDecrypt(toDecrypt, decipherPass.getText(), Cipher.DECRYPT_MODE);
         } else {
             showAlert();
         }
@@ -87,6 +97,60 @@ public class CipherDecipherController {
         decipherFileAddress.setText("");
         decipherPass.setText("");
         decipherPassConfirmed.setText("");
+    }
+
+    private void encryptDecrypt(File toEncrypt, String password, int type) {
+        try {
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(toEncrypt));
+            BufferedOutputStream out;
+            File toOut = null;
+            if (type == Cipher.ENCRYPT_MODE){
+                toOut = new File("encrypted.txt");
+                out = new BufferedOutputStream(new FileOutputStream(toOut));
+            }else{
+                toOut = new File("decrypted.txt");
+                out = new BufferedOutputStream(new FileOutputStream(toOut));
+            }
+            SecretKeyFactory kf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] salt = new byte[20];
+            byte[] iv = new byte[16];
+            KeySpec ks = new PBEKeySpec(password.toCharArray(), salt, 1000, 128);
+            SecretKey key = kf.generateSecret(ks);
+            byte[] aesKey = key.getEncoded();
+            IvParameterSpec ivspec = new IvParameterSpec(iv);
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            SecretKeySpec keySpec = new SecretKeySpec(aesKey, "AES");
+            cipher.init(type, keySpec, ivspec);
+            byte[] inputBuffer = new byte[1];
+            int bytesRead = in.read(inputBuffer);
+            while (bytesRead > 0) {
+                out.write(cipher.update(inputBuffer, 0, bytesRead));
+                bytesRead = in.read(inputBuffer);
+            }
+            out.write(cipher.doFinal());
+            in.close();
+            Runtime.getRuntime().exec("explorer.exe /select," + toOut.getAbsolutePath());
+            out.close();
+            cleanFields();
+        }catch (FileNotFoundException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error");
+            alert.setContentText("No se encontró el archivo.");
+            alert.showAndWait();
+            cleanFields();
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException |
+                InvalidKeyException | IOException | BadPaddingException | IllegalBlockSizeException |
+                InvalidKeySpecException e){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error");
+            alert.setContentText("Error al cifrar.");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+            cleanFields();
+        }
+
     }
 
 }
